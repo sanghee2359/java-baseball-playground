8777b57d74a5c2d781216aec9b94f16f817e3a91
wjdtk <wjdtkdgml7352@naver.com>
2024-08-05 20:09:12 +0900

    feat. 사용자 입력값의 검증 추가

---
+++ src/main/java/numbersBaseball/ValidationError.java
@@ -0,0 +1,7 @@
+package numbersBaseball;
+
+public class ValidationError {
+    public static String intermediateValue = "1 ~ 9 사이의 숫자를 입력해주세요.";
+    public static String duplication = "중복된 값 없이 입력해주세요.";
+
+}
