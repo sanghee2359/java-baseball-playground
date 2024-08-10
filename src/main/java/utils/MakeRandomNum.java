bf730edf4abd49cedca733335e64ebe564d7d36b
wjdtk <wjdtkdgml7352@naver.com>
2024-08-10 20:20:59 +0900

    feat. 3자리의 랜덤숫자 생성

---
+++ src/main/java/utils/MakeRandomNum.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MakeRandomNum {
+    private List<Integer> numList;
+    private static boolean[] check; // true이면 값이 들어가있다
+    public MakeRandomNum() {
+        check = new boolean[10]; // 1~9 사이의 수 파악
+    }
+    public List<Integer> makeRandomNum(){
+        numList = new ArrayList<>();
+        Random random = new Random();
+        int idx = 0;
+        while(idx != 3) {
+            int num = random.nextInt(9)+1; // 1 ~ 4 까지의 무작위 int 값 리턴
+            idx = checkDuplicate(idx, num);
+        }
+        System.out.println(numList.toString());
+        return numList;
+    }
+    public int checkDuplicate(int idx, int num) {
+        if(!check[num]) {
+            idx++;
+            check[num] = true;
+            numList.add(num);
+        }
+        return idx;
+    }
+}
